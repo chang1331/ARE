3ce1d8137ba79b51589716437e3338c7c379de36
chang1331 <chang1.3@outlook.com>
2018-04-29 19:05:44 -0500

    Funciona (falta condición mayor)

--- ARE/Form1.Designer.cs
+++ ARE/Form1.Designer.cs
@@ -31,25 +31,18 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmSalones));
             this.btnCalcula = new System.Windows.Forms.Button();
             this.txtClases = new System.Windows.Forms.TextBox();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.txtMaxSalones = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.rBMaestro = new System.Windows.Forms.RadioButton();
-            this.rBHorario = new System.Windows.Forms.RadioButton();
             this.label2 = new System.Windows.Forms.Label();
-            this.cBSalones = new System.Windows.Forms.ComboBox();
-            this.label7 = new System.Windows.Forms.Label();
-            this.txtNRes = new System.Windows.Forms.TextBox();
-            this.cbRestriccion = new System.Windows.Forms.ComboBox();
             this.label8 = new System.Windows.Forms.Label();
             this.txtnumClases = new System.Windows.Forms.TextBox();
             this.label9 = new System.Windows.Forms.Label();
-            this.btnRestricciones = new System.Windows.Forms.Button();
+            this.txtResMaes = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtResHor = new System.Windows.Forms.TextBox();
+            this.btnllenaDef = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnCalcula
@@ -73,27 +66,6 @@
             this.txtClases.Size = new System.Drawing.Size(417, 88);
             this.txtClases.TabIndex = 1;
             this.txtClases.WordWrap = false;
-            // 
-            // listBox1
-            // 
-            this.listBox1.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 21;
-            this.listBox1.Location = new System.Drawing.Point(678, 275);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(221, 130);
-            this.listBox1.TabIndex = 2;
-            // 
-            // button1
-            // 
-            this.button1.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(960, 341);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(107, 67);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Eliminar restricción";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
@@ -141,56 +113,6 @@
             this.label4.Text = "Número de salones:";
             this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.BackColor = System.Drawing.Color.Transparent;
-            this.label6.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label6.Location = new System.Drawing.Point(674, 146);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(54, 21);
-            this.label6.TabIndex = 19;
-            this.label6.Text = "Tipo: ";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.BackColor = System.Drawing.Color.Transparent;
-            this.label5.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label5.Location = new System.Drawing.Point(674, 107);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(67, 21);
-            this.label5.TabIndex = 18;
-            this.label5.Text = "Clase: ";
-            // 
-            // rBMaestro
-            // 
-            this.rBMaestro.AutoSize = true;
-            this.rBMaestro.BackColor = System.Drawing.Color.Transparent;
-            this.rBMaestro.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.rBMaestro.Location = new System.Drawing.Point(1015, 148);
-            this.rBMaestro.Name = "rBMaestro";
-            this.rBMaestro.Size = new System.Drawing.Size(80, 21);
-            this.rBMaestro.TabIndex = 17;
-            this.rBMaestro.TabStop = true;
-            this.rBMaestro.Text = "Maestro";
-            this.rBMaestro.UseVisualStyleBackColor = false;
-            // 
-            // rBHorario
-            // 
-            this.rBHorario.AutoSize = true;
-            this.rBHorario.BackColor = System.Drawing.Color.Transparent;
-            this.rBHorario.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.rBHorario.Location = new System.Drawing.Point(868, 148);
-            this.rBHorario.Name = "rBHorario";
-            this.rBHorario.Size = new System.Drawing.Size(76, 21);
-            this.rBHorario.TabIndex = 16;
-            this.rBHorario.TabStop = true;
-            this.rBHorario.Text = "Horario";
-            this.rBHorario.UseVisualStyleBackColor = false;
-            // 
             // label2
             // 
             this.label2.AutoSize = true;
@@ -199,47 +121,9 @@
             this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label2.Location = new System.Drawing.Point(684, 54);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(234, 29);
+            this.label2.Size = new System.Drawing.Size(350, 29);
             this.label2.TabIndex = 15;
-            this.label2.Text = "Agrega restricciones";
-            // 
-            // cBSalones
-            // 
-            this.cBSalones.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cBSalones.FormattingEnabled = true;
-            this.cBSalones.Location = new System.Drawing.Point(866, 99);
-            this.cBSalones.Name = "cBSalones";
-            this.cBSalones.Size = new System.Drawing.Size(52, 29);
-            this.cBSalones.TabIndex = 14;
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.BackColor = System.Drawing.Color.Transparent;
-            this.label7.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label7.Location = new System.Drawing.Point(674, 184);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(104, 21);
-            this.label7.TabIndex = 20;
-            this.label7.Text = "Restricción:";
-            // 
-            // txtNRes
-            // 
-            this.txtNRes.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtNRes.Location = new System.Drawing.Point(1015, 185);
-            this.txtNRes.Name = "txtNRes";
-            this.txtNRes.Size = new System.Drawing.Size(52, 28);
-            this.txtNRes.TabIndex = 22;
-            // 
-            // cbRestriccion
-            // 
-            this.cbRestriccion.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cbRestriccion.FormattingEnabled = true;
-            this.cbRestriccion.Location = new System.Drawing.Point(868, 184);
-            this.cbRestriccion.Name = "cbRestriccion";
-            this.cbRestriccion.Size = new System.Drawing.Size(109, 29);
-            this.cbRestriccion.TabIndex = 23;
+            this.label2.Text = "Agrega restricciones de horario";
             // 
             // label8
             // 
@@ -274,15 +158,49 @@
             this.label9.TabIndex = 24;
             this.label9.Text = "Escribe el número de clases";
             // 
-            // btnRestricciones
+            // txtResMaes
             // 
-            this.btnRestricciones.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRestricciones.Location = new System.Drawing.Point(960, 275);
-            this.btnRestricciones.Name = "btnRestricciones";
-            this.btnRestricciones.Size = new System.Drawing.Size(135, 60);
-            this.btnRestricciones.TabIndex = 27;
-            this.btnRestricciones.Text = "Agregar Restricciones";
-            this.btnRestricciones.UseVisualStyleBackColor = true;
+            this.txtResMaes.Location = new System.Drawing.Point(689, 301);
+            this.txtResMaes.Multiline = true;
+            this.txtResMaes.Name = "txtResMaes";
+            this.txtResMaes.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtResMaes.Size = new System.Drawing.Size(276, 88);
+            this.txtResMaes.TabIndex = 29;
+            this.txtResMaes.WordWrap = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label6.Location = new System.Drawing.Point(684, 269);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(361, 29);
+            this.label6.TabIndex = 32;
+            this.label6.Text = "Agrega restricciones de maestro";
+            // 
+            // txtResHor
+            // 
+            this.txtResHor.Location = new System.Drawing.Point(689, 104);
+            this.txtResHor.Multiline = true;
+            this.txtResHor.Name = "txtResHor";
+            this.txtResHor.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtResHor.Size = new System.Drawing.Size(276, 88);
+            this.txtResHor.TabIndex = 33;
+            this.txtResHor.WordWrap = false;
+            // 
+            // btnllenaDef
+            // 
+            this.btnllenaDef.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnllenaDef.BackColor = System.Drawing.Color.Transparent;
+            this.btnllenaDef.Location = new System.Drawing.Point(488, 469);
+            this.btnllenaDef.Name = "btnllenaDef";
+            this.btnllenaDef.Size = new System.Drawing.Size(191, 23);
+            this.btnllenaDef.TabIndex = 34;
+            this.btnllenaDef.Text = "LlenaDefault";
+            this.btnllenaDef.UseVisualStyleBackColor = false;
+            this.btnllenaDef.Click += new System.EventHandler(this.btnllenaDef_Click);
             // 
             // frmSalones
             // 
@@ -291,25 +209,18 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1152, 658);
-            this.Controls.Add(this.btnRestricciones);
+            this.Controls.Add(this.btnllenaDef);
+            this.Controls.Add(this.txtResHor);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtResMaes);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.txtnumClases);
             this.Controls.Add(this.label9);
-            this.Controls.Add(this.cbRestriccion);
-            this.Controls.Add(this.txtNRes);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.rBMaestro);
-            this.Controls.Add(this.rBHorario);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.cBSalones);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.txtMaxSalones);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.txtClases);
             this.Controls.Add(this.btnCalcula);
             this.Name = "frmSalones";
@@ -324,25 +235,18 @@
 
         private System.Windows.Forms.Button btnCalcula;
         private System.Windows.Forms.TextBox txtClases;
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox txtMaxSalones;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.RadioButton rBMaestro;
-        private System.Windows.Forms.RadioButton rBHorario;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ComboBox cBSalones;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.TextBox txtNRes;
-        private System.Windows.Forms.ComboBox cbRestriccion;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.TextBox txtnumClases;
         private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.Button btnRestricciones;
+        private System.Windows.Forms.TextBox txtResMaes;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtResHor;
+        private System.Windows.Forms.Button btnllenaDef;
     }
 }
 
--- ARE/Form1.cs
+++ ARE/Form1.cs
@@ -12,11 +12,15 @@ namespace ARE
 {
     public partial class frmSalones : Form
     {
+
         int[] salones;
+        List<List<int>> horario = new List<List<int>>();
+        List<int>[] resHorario;
+       // Dictionary<int,int> resHorario= new Dictionary<int, int>() ;
         Boolean[,] adyacencia;
         String[] linea;
         char c;
-        int indexN,indexAdj;
+        int indexN,indexAdj,index, n,resSalon ;
 
         public frmSalones()
         {
@@ -40,7 +44,7 @@ namespace ARE
 
         private void frmSalones_Load(object sender, EventArgs e)
         {
-
+            
         }
 
         private void rBMaestro_CheckedChanged(object sender, EventArgs e)
@@ -55,7 +59,7 @@ namespace ARE
 
         private void btnCalcula_Click(object sender, EventArgs e)
         {
-            int n = Int16.Parse(txtnumClases.Text);
+            n = Int16.Parse(txtnumClases.Text);
 
             adyacencia= new Boolean[n,n];
             for (int i=0; i<txtClases.Lines.Length; i++)
@@ -83,6 +87,91 @@ namespace ARE
 
             }
 
+            //agrego restricciones de maestro
+            
+             for (int i = 0; i < txtResMaes.Lines.Length; i++)
+            {
+                linea = txtResMaes.Lines[i].Split('-'); //clique a array
+                c = Convert.ToChar(linea[0]);        //convierte a char el primer elemento del clique
+                indexN = char.ToUpper(c) - 65;       //convierte a indice el char
+
+                for (int j = 1; j < linea.Length; j++)
+                {
+                    if (linea.Length == 2)
+                    {
+                        c = Convert.ToChar(linea[j]);
+                        indexAdj = char.ToUpper(c) - 65;
+
+                        adyacencia[indexN, indexAdj] = true;
+                        adyacencia[indexAdj, indexN] = true;
+                    }
+                    else
+                    {
+                        conectaN(linea);//conecta todos con todos del clique
+                    }
+
+                }
+
+            }
+
+            //inicio la matriz de salon con restriccion, y la lleno con n listas
+            resHorario = new List<int>[n];
+
+            for (int i=0;i<n;i++)
+            {
+                resHorario[i]=new List<int>();
+            }
+
+            for (int j = 0; j < txtResHor.Lines.Length; j++)
+            {
+                String linea = txtResHor.Lines[j];
+                
+                if (linea[1] == '<')
+                {
+                    c = Convert.ToChar(linea[0]);
+                    index = char.ToUpper(c) - 65;
+
+                    Console.WriteLine(index);
+                    for (int i = (int)Char.GetNumericValue(linea[3])-2 ; i > 0; i--)
+                    {
+                        resHorario[index].Add(i);
+                    }
+                }
+                //si es mayor no hay que llenar el arreglo con for
+                else
+                {
+                    index = (int)Convert.ToChar(linea[0])-65;
+                    Console.WriteLine(index);
+                    for (int i=0; i < (int)Char.GetNumericValue(linea[3])-1; i++) {
+                        resHorario[index].Add(i);
+                    }
+                }
+            }
+
+            salones = new int[n];
+
+            for(int i = 0; i < n; i++)
+             {
+                for(int j=0; j<n; j++)
+                {
+                    if (adyacencia[i,j])
+                    {
+                        salones[i]++;
+                    }
+                }
+            }
+            Console.WriteLine("salones numero de conex");
+            for(int i = 0; i < n; i++)
+            {
+                Console.Write(salones[i]);
+            }
+            resSalon= Int16.Parse(txtMaxSalones.Text);
+            horario.Add(new List<int>());
+            //agrego las restricciones de horario a horario
+            
+
+            areBi();
+
             //imprime matriz
             for (int i = 0; i < n; i++)
             {
@@ -93,18 +182,28 @@ namespace ARE
                 Console.Write(Environment.NewLine);
             }
 
+            for (int i = 0; i < horario.Count; i++)
+            {
+                for (int j = 0; j < horario[i].Count; j++)
+                {
+                    Console.Write(string.Format("{0} ", horario[i][j]));
+                }
+                Console.Write(Environment.NewLine);
+            }
+
+
+
         }
-/*A-E
-B-C
-C-D
-E-D
-E-D
-A-B-D
-A-H-I
-F-G-H
-B-H-I
-D-F-I
-D-H-I*/
+
+        private void btnllenaDef_Click(object sender, EventArgs e)
+        {
+            txtnumClases.Text = "9";
+            txtClases.Text = "A-E\r\nB-C\r\nC-D\r\nE-D\r\nA-B-D\r\nA-H-I\r\nF-G-H\r\nB-H-I\r\nD-F-I\r\nD-H-I";
+            txtMaxSalones.Text = "2";
+            txtResHor.Text = "A<=3";
+
+        }
+
         //conecta n nodos en un n-clique con n>2
         public void conectaN(String[] l)
         {
@@ -131,24 +230,74 @@ D-H-I*/
 
         public void areBi()
         {
+            for (int k=0;k<n;k++) {
+              
+                Console.WriteLine("k " + k);
+                int iMax = obtenerMaxIndex();
+                Console.WriteLine("indice de salon con mas conex " + iMax);
+                int x = 0;
+                Console.WriteLine("contains " + resHorario[iMax].Contains(x));
+                Console.WriteLine("count " + (horario[x].Count >= resSalon));
+                while (resHorario[iMax].Contains(x) || horario[x].Count >= resSalon)
+                {
+                    x++;
+                    Console.WriteLine("tam horario " + horario.Count + "\n x " + x);
+                    if (x >= horario.Count)
+                    {
+                        horario.Add(new List<int>());
+                    }
+                    Console.WriteLine("tam h antes de if " + horario.Count);
+                    Console.WriteLine("contains siguiente " + resHorario[iMax].Contains(x));
+                    Console.WriteLine("count siguiente " + (horario[x].Count >= resSalon));
+                }
+                horario[x].Add(iMax);
+                Console.WriteLine("agregué " + iMax + " a horario " + x);
 
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-            char l;
-            cBSalones.Items.Clear();
-            if (txtnumClases.Text != "")
-            {
-                int n = Int16.Parse(txtnumClases.Text);
                 for (int i = 0; i < n; i++)
                 {
-                    l = (char)(i + 65);
-                    l = Convert.ToChar(l);
-                    cBSalones.Items.Add(l);
+                    if (adyacencia[iMax,i])
+                    {
+                        salones[i]--;
+                        resHorario[i].Add(x);
+                    }
+                }
+
+                //elimino el salón
+                salones[iMax] = -100;
+
+                Console.WriteLine("salones numero de conex");
+                for (int i = 0; i < n; i++)
+                {
+                    Console.Write(salones[i]);
                 }
 
             }
+            
+        }
+
+        public int obtenerMaxIndex()
+        {
+            int max=0, maxIndex=0;
+            for (int i = 0; i < n; i++)
+            {
+                if (salones[i] == 0)
+                {
+                    max = salones[i];
+                    maxIndex = i;
+                    break;
+                }
+               else if (salones[i] > max) {
+                    max = salones[i];
+                    maxIndex =  i;
+                }   
+              
+            }
+            return maxIndex;
+        }
+        
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
 
         }
     }
